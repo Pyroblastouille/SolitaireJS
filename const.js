8e303209d9e14b727f9b853f1f32b487efe71de1
TheMrChipsman <gael.mariot@gmail.com>
2019-12-02 16:04:46 +0100

    Cartes corrigées et Request de mouvement

--- const.js
+++ const.js
@@ -1,101 +1,120 @@
 var VALEURS = {
-    A:1,
-    1:1,
-    2:2,
-    3:3,
-    4:4,
-    5:5,
-    6:6,
-    7:7,
-    8:8,
-    9:9,
-    10:10,
-    J:11,
-    Q:12,
-    K:13
+    As: 1,
+    Deux: 2,
+    Trois: 3,
+    Quatre: 4,
+    Cinq: 5,
+    Six: 6,
+    Sept: 7,
+    Huit: 8,
+    Neuf: 9,
+    Dix: 10,
+    Valet: 11,
+    Dame: 12,
+    Roi: 13
 };
 
 var COULEURS = {
-    ROUGE:{CARREAU:1,COEUR:2},
-    NOIR:{PIQUE:3,TREFLE:4}
+    PIQUE: 1,
+    CARREAU: 2,
+    TREFLE: 3,
+    COEUR: 4
 }
 
-class Deck{
+class Deck {
     constructor(defaultDeck = false) {
         this.cartes = [];
-        if(defaultDeck){
+        if (defaultDeck) {
             this.DefaultDeck();
         }
     }
 
-    DefaultDeck(){
+    DefaultDeck() {
         this.Empty();
         COULEURS.forEach(col => {
             VALEURS.forEach(val => {
-                this.Add(new Card(val,col));
+                this.Add(new Card(val, col));
             });
         });
     }
-    Empty(){
+    Empty() {
         this.cartes = [];
     }
     /**
      * Retourne le nombre de cartes dans le deck
      */
-    DeckSize(){
+    DeckSize() {
         return this.cartes.length;
     }
     /**
      * Tire la dernière carte du deck
      */
-    Draw(){
+    Draw() {
         return this.cartes.pop();
+    }
+    /**
+     * 
+     */
+    Last() {
+        return this.cartes[this.DeckSize() - 1];
     }
     /**
      * Rajoute une carte dans le deck
      * @param {Card} aCarte 
      */
-    Add(aCarte){
+    Add(aCarte) {
         this.cartes.push(aCarte);
     }
     /**
      * Mélange le deck
      */
-    Shuffle(){
+    Shuffle() {
         this.cartes.sort(() => 0.5 - Math.random());
     }
 }
 
-class Card{
-    constructor(Valeur,Couleur) {
+class Card {
+    constructor(Valeur, Couleur) {
         this.couleur = Couleur;
         this.valeur = Valeur;
         this.visible = false;
     }
-    Flip(){
+    Flip() {
         this.visible = !this.visible;
     }
     /* Check for values */
-    EqualValue(than){
+    EqualValue(than) {
         return (this.valeur == than.valeur);
     }
-    JustMoreValue(than){
-        return (this.valeur-than.valeur == -1);
+    JustMoreValue(than) {
+        return (this.valeur - than.valeur == 1);
     }
-    MoreValue(than){
-        return (this.valeur-than.valeur < 0);
+    MoreValue(than) {
+        return (this.valeur - than.valeur > 0);
     }
-    JustLessValue(than){
-        return (this.valeur-than.valeur == 1);
+    JustLessValue(than) {
+        return (this.valeur - than.valeur == -1);
     }
-    LessValue(than){
-        return (this.valeur-than.valeur > 0);
+    LessValue(than) {
+        return (this.valeur - than.valeur < 0);
     }
     /* Check for Colors */
-    SameColor(than){
-        return this.couleur == than.couleur;
+    SameColor(than) {
+        switch (this.couleur) {
+            case COULEURS.CARREAU:
+            case COULEURS.COEUR:
+                return (than.couleur == COULEURS.CARREAU || than.couleur == COULEURS.COEUR);
+                break;
+            case COULEURS.PIQUE:
+            case COULEURS.TREFLE:
+                return (than.couleur == COULEURS.PIQUE || than.couleur == COULEURS.TREFLE);
+                break;
+            default:
+                return false;
+                break;
+        }
     }
-    DifferentColor(than){
+    DifferentColor(than) {
         return !this.SameColor(than)
     }
 }
--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var NB_OUT = 4;
 
 var pioche, colonnes, sorties;
 
-function Setup(){
+function Setup() {
     pioche = new Deck(true);
     colonnes = [];
     sorties = [];
     //Définit les decks de sortie
     for (let k = 0; k < NB_OUT; k++) {
-        sorties.push(new Deck()); 
+        sorties.push(new Deck());
     }
     //Définit les colonnes du jeu
     for (let i = 0; i < NB_COLUMNS; i++) {
@@ -20,6 +20,35 @@ function Setup(){
     }
 }
 
-function RequestMove(fromCol,atId,toCol){
-    if(colonnes[fromCol].cartes[atId].valeur)
+/**
+ * 
+ * @param {int} fromCol 
+ * @param {int} atId 
+ * @param {int} toCol 
+ */
+function RequestMove(fromCol, atId, toCol) {
+    return (colonnes[fromCol].cartes[atId].JustLessThan(colonnes[toCol].Last()));
+}
+/**
+ * 
+ * @param {Card} card 
+ */
+function RequestOut(card) {
+    switch (card.couleur) {
+        case COULEURS.PIQUE:
+            return (card.JustMoreValue(sorties[0].Last()));
+            break;
+        case COULEURS.CARREAU:
+            return (card.JustMoreValue(sorties[1].Last()));
+            break;
+        case COULEURS.TREFLE:
+            return (card.JustMoreValue(sorties[2].Last()));
+            break;
+        case COULEURS.COEUR:
+            return (card.JustMoreValue(sorties[3].Last()));
+            break;
+        default:
+            return false;
+            break;
+    }
 }
